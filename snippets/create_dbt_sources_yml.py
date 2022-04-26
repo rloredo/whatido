143e7fbc945c9243898da348c2194dc1235de1a8
Rodrigo Loredo <rodrigo@amberscript.com>
2022-04-26 11:53:13 +0200

    refactor to class and use dicts instead of strings

--- snippets/create_dbt_sources_yml.py
+++ snippets/create_dbt_sources_yml.py
@@ -1,34 +1,66 @@
-#Based by: https://github.com/dbt-labs/dbt-core/issues/1082
-from sqlalchemy import create_engine
-import pandas as pd 
-conn = create_engine('YOUR DB CONNECTION STRING HERE')
+#Based on: https://github.com/dbt-labs/dbt-core/issues/1082
+import pandas as pd
+import yaml
 
-db_metadata = pd.read_sql_query("""SELECT table_schema, 
-                                          table_name,
-                                          column_name,
-                                          udt_name AS column_data_type
-                                    FROM information_schema."columns"
-                                 """, conn)
+class Source:
+    def __init__(self, db_connection_engine):
+        self.conn = db_connection_engine
+        self.sources_metadata_df = self._get_sources_df()
 
-for schema in db_metadata.table_schema.unique():
-    tmp_str = f"version: 2\n\nsources:\n  - name: {schema}\n    tables:\n"
+    def _get_sources_df(self) -> pd.DataFrame:
+        """
+        Get all source metadata from a database.
+        """
+        db_metadata = pd.read_sql_query("""SELECT table_schema, 
+                                                  table_name,
+                                                  column_name,
+                                                  udt_name AS column_type
+                                            FROM information_schema."columns"
+                                            """, self.conn)
+
+        return db_metadata
     
-    for table in db_metadata.loc[db_metadata.table_schema == schema].table_name.sort_values().unique():
-        tmp_str = f"{tmp_str}      - name: {table}\n"
-        tmp_str = f"{tmp_str}        description: ''\n"
-        tmp_str = f"{tmp_str}        tests:\n"
-        tmp_str = f"{tmp_str}        columns:\n"
-        
-        for column in db_metadata.loc[db_metadata.table_name == table].column_name:
-            tmp_str = f"{tmp_str}         - name: {column}\n"
-            tmp_str = f"{tmp_str}           description: ''\n"
-            tmp_str = f"{tmp_str}           tests:\n"
-            #TODO add column data type
-            
-    #Write each schema to a yml file
-    with open(f"PATH_TO_SOURCES_FOLDER/{schema}.yml", 'w') as ymlfile:
-        ymlfile.write(tmp_str)
-        ymlfile.close()      
-    
-    #TODO instead of strings, create dicts. It will be useful to compare and update
-     
+    def get_available_schemas(self)->set:
+        """
+        Return available schemas of the database
+        Useful for exclude param
+        """
+
+        return set(self.sources_metadata_df.table_schema.unique())
+
+    def get_sources_dict(self, exclude:list = []) -> dict:
+        """
+        Return a dict with the schema name as key and metadata dictionary as value
+        Exclude: list of schemas to exclude
+        """
+        sources_dict = {}
+        for schema in self.sources_metadata_df.loc[~self.sources_metadata_df.table_schema.isin(exclude)].table_schema.unique():
+            tmp_table_list = []
+            for table in self.sources_metadata_df.loc[self.sources_metadata_df.table_schema == schema].table_name.sort_values().unique():
+                tmp_column_list = []
+                for column in self.sources_metadata_df.loc[(self.sources_metadata_df.table_name == table) & (self.sources_metadata_df.table_schema == schema)].column_name.sort_values():                
+                    tmp_column_list.append({"name":column, "description": "", "tests": None})
+                tmp_table_list.append({"name": table, "description":"", "tests": None, "columns": tmp_column_list})
+            sources_dict.update({f"{schema}": {"version":2, "sources": [{"name": schema, "tables": tmp_table_list}]}})
+        return sources_dict
+
+    def write_to_yml(self, sources_folder_path:str, exclude:list = [])->None:
+        """
+        Write one yml file per schema in the database
+        """
+        sources_dict = self.get_sources_dict(exclude)
+        for schema in sources_dict.keys():
+            with open(f'{sources_folder_path}/{schema}.yml', 'w') as outfile:
+                yaml.dump(sources_dict.get(schema), outfile, Dumper=IndentedDumper, sort_keys=False)
+       
+#Solution to indent the yaml properly.
+#See: https://stackoverflow.com/a/39681672
+class IndentedDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(IndentedDumper, self).increase_indent(flow, False)
+
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    conn = create_engine('YOUR DB CONNECTION STRING HERE')
+    MyDB = Source(conn)  
+    MyDB.write_to_yml('.')
